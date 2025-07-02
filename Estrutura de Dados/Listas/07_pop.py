8bf4f14448401a5947b38522ad9b607768c154fc
Gabriel <gbr.vianas@gmail.com>
2025-07-02 00:35:02 -0300

    Estrura de Dados - Python

---
+++ Estrutura de Dados/Listas/07_pop.py
@@ -0,0 +1,10 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.pop())  # csharp
+print(linguagens.pop())  # java
+print(linguagens.pop())  # c
+print(linguagens.pop(0))  # python
+
+#Quando você não passa um índice, como
+#linguagens.pop()
+#Ele remove o último elemento da lista comportamento padrão da pilha 
