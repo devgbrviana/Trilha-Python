072d92f42da76e329cefb9c9bb4f30f707b57a83
Gabriel <gbr.vianas@gmail.com>
2025-07-02 00:42:09 -0300

    sort

---
+++ Estrutura de Dados/Listas/09_sort.py
@@ -0,0 +1,25 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+print(linguagens)
+
+#sort() → ordem alfabética crescente
+
+#sort(reverse=True) → ordem alfabética decrescente
+
+#sort(key=...) → define uma função de ordenação personalizada
+
+#sort(key=..., reverse=True) → personaliza e inverte a ordem
+
+#lambda e uma forma de criar funoes pequenas, sem nome lambda argumentos: expressão
